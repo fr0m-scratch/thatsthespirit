658b49cf3e8d28712272fea8e1fb671bcba65a6a
fr0m-scratch <jamaltheodds@gmail.com>
2023-03-18 19:58:30 -0400

    major del

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,5 @@
 const headerNavLinks = [
-  { href: '/blog', title: '文章' },
-  { href: '/tags', title: '标签' },
-  { href: '/video', title: '视频' },
-  { href: '/projects', title: '项目' },
-  { href: 'https://www.runjs.cool/', title: '工具' },
+  { href: '/video', title: 'episodes' },
   { href: '/about', title: '关于' },
 ]
 
